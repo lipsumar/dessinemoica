9726bd1c8408b3c6cc5a429b6fc1518538d0a204
Emmanuel Pire <piremmanuel@gmail.com>
2020-05-09 17:21:15 +0200

    fix

--- post-now.js
+++ post-now.js
@@ -1,5 +1,5 @@
 const { postTweet } = require('./twitterLib');
-
+const axios = require('axios')
 
 async function getText(){
   const resp = await axios.get(`${process.env.AUTOMOTRON_BASE_URL}/api/generators/h2VfKhAF0/run`)
